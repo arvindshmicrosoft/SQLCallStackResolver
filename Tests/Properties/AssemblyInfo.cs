5fbf0b5a29e7e91b6623a9ff2f92728f068dfef7
Arvind Shyamsundar <arvindsh@microsoft.com>
2019-05-24 15:38:43 -0700

    Project restructure - Output folder normalized - XELite latest .NET Fx 4.6.1 nuget - Removed changes which are not needed anymore - Set version to 1.5.0.0 and updated copyright etc.

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SQLCallStackResolver.Tests")]
+[assembly: AssemblyDescription("Tests for SQLCallStackResolver Engine")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("SQLCallStackResolver")]
+[assembly: AssemblyCopyright("Copyright © Arvind Shyamsundar 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
